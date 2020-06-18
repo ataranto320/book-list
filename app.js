d180b52afbb8404468d0d93aa6ab42d03c1b8090
Austin T <austin.taranto@gmail.com>
2020-06-18 14:30:21 -0400

    variables, ui function

--- app.js
+++ app.js
@@ -7,10 +7,27 @@ function Book(title, author, isbn) {
 
 // ui constructor
 function UI() {}
+// add book to list
+UI.prototype.addBookToList = function(book) {
+    // console.log(book);
+}
 
 // event listeners
 document.getElementById("book-form").addEventListener("submit", function(e) {
     // console.log("test");
+    // get form values
+    const title = document.getElementById("title").nodeValue,
+    author = document.getElementById("author").nodeValue,
+    isbn = document.getElementById(isbn).value
+    // console.log(title, author, isbn);
+    // instantiate book
+    const book = new Book(title, author, isbn);
+    // console.log(book);
+    // instantiate ui
+    const ui = new UI();
+    // console.log(ui);
+    // add book to list
+    ui.addBookToList(book);
 
     e.preventDefault();
 });
