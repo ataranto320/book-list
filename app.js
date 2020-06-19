eb7bf4a98f1dd3261183dd126b4d35fffa762829
Austin T <austin.taranto@gmail.com>
2020-06-19 14:46:47 -0400

    show alert function

--- app.js
+++ app.js
@@ -25,6 +25,26 @@ UI.prototype.addBookToList = function(book) {
     list.appendChild(row);
 }
 
+// show alert
+UI.prototype.showAlert = function(message, className) {
+    // create div
+    const div = document.createElement("div");
+    // add classes
+    div.className = `alert ${className}`;
+    // add text
+    div.appendChild(document.createTextNode(message));
+    // get parent 
+    const container = document.querySelector(".container");
+    // get form
+    const form = document.querySelector("#book-form");
+    // insert alert
+    container.insertBefore(div, form);
+    // timeout after 3 secs
+    setTimeout(function() {
+        document.querySelector(".alert").remove();
+    }, 3000);
+}
+
 // clear fields 
 UI.prototype.clearFields = function() {
     document.getElementById("title").value = "";
@@ -50,7 +70,7 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
     if (title === "" || author === "" || isbn === "") {
         // alert("failed");
         // error alert 
-        UI.showAlert("please fill in all fields", "error");
+        ui.showAlert("please fill in all fields", "error");
     } else {
     // add book to list
     ui.addBookToList(book);
